684f30104a498f2a1e86623c4c66edadb0c11fb9
gaa0 <lotte1071103@naver.com>
2021-08-03 11:20:25 +0900

    flask set

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+from flask import Flask, render_template
 from pymongo import MongoClient
+
+app = Flask(__name__)
+
 client = MongoClient('localhost', 27017)
 db = client.dbweather
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
