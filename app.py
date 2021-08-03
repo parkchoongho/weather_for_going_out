1681bcf2f6bc25a715946db140f1298a9ff364d6
KANGHO KIM <Dean@kimhyuntaes-MacBook-Air.local>
2021-08-03 20:50:51 +0900

    join2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+import requests
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -8,22 +9,31 @@ db = client.dbweather
 
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+@app.route('/join', methods=['GET'])
+def get_join():
     return render_template('join.html')
 
-@app.route('/signUp', methods=['POST'])
-def post_signUp():
+@app.route('/', methods=['GET'])
+def dropdown():
+    colours = 
+
+@app.route('/join', methods=['POST'])
+def post_join():
     # 1. 클라이언트로부터 데이터를 받기
     userID_receive = request.form['userID_give']  # 클라이언트로부터 url을 받는 부분
     pw_receive = request.form['pw_give']  # 클라이언트로부터 comment를 받는 부분
     pw2_receive = request.form['pw2_give']  # 클라이언트로부터 comment를 받는 부분
     area_receive = request.form['area_give']  # 클라이언트로부터 comment를 받는 부분
     goingToOffice_receive = request.form['goingToOffice_give']
-    goingHome_receive = request.form['goingHome']
+    goingToOffice_receive2 = goingToOffice_receive[0:2]
+    goingHome_receive = request.form['goingHome_give']
+    goingHome_receive2 = goingHome_receive[0:2]
 
-    signUp = {'userID': userID_receive, 'pw': pw_receive, 'pw2': pw2_receive,'area': area_receive, 
-              'goingToOffice': goingToOffice_receive, 'goingHome': goingHome}
+    join = {'userID': userID_receive, 'pw': pw_receive, 'pw2': pw2_receive,'area': area_receive, 'goingToOffice': goingToOffice_receive2, 'goingHome': goingHome_receive2}
 
-    db.dbweather.insert_one(signUp)
+    db.dbweather.insert_one(join)
 
     return jsonify({'result': 'success'})
 
