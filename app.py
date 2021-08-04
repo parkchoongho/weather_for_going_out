95f8eb3475961ddcea24c007be47c904ef6a04ab
gaa0 <lotte1071103@naver.com>
2021-08-04 17:42:32 +0900

    충돌 해결 중

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def main():
         state_list_t = []
 
         for item in items['item']:
-            if item['fcstDate'] == today_date and item['fcstTime'] in [goingToOffice, goingToOfficeEnd, goingHome, goingHomeEnd]:
+            if item['fcstDate'] == today_date and item['fcstTime'] in [goingToOffice, goingToOfficeEnd, goingHome, goingHomeEnd]: 
                 # 기온
                 if item['category'] == 'TMP':
                     print(goingToOffice, goingToOfficeEnd)
