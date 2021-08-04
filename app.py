f91ff4bf9f282b012fafa62b31a85fb391686224
gaa0 <lotte1071103@naver.com>
2021-08-04 17:32:39 +0900

    충돌 해결 중

--- app.py
+++ app.py
@@ -305,7 +305,7 @@ def main():
     
     
 
-    return render_template('index.html', max_TMP=max_TMP, min_TMP=min_TMP, umbrella=umbrella, clothes_txt=clothes_txt, msg=msg, img=img, max_TMP_t=max_TMP_t, min_TMP_t=min_TMP_t, umbrella_t=umbrella_t, clothes_txt_t=clothes_txt_t, msg_t=msg_t, img_t=img_t)
+    return render_template('index.html', max_TMP=max_TMP, min_TMP=min_TMP, umbrella=umbrella, clothes_txt=clothes_txt, msg=msg, img=img, max_TMP_t=max_TMP_t, min_TMP_t=min_TMP_t, umbrella_t=umbrella_t, clothes_txt_t=clothes_txt_t, msg_t=msg_t, img_t=img_t )
 
 
 
